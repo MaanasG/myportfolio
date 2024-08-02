457ac2fce6a2c2d13da8cc11ee3febbd2350a15f
MaanasG <gopimaanas@gmail.com>
2024-08-02 18:39:30 -0400

    nav dropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -23,19 +23,17 @@ const Navbar = () => {
             <div className="navbar--menu-container" onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
                 <h2 className="navbar--menu">Menu</h2>
                 <div className={`navbar--dropdown ${isHover ? 'show' : ''}`}>
-                    <Slide>
-                        <ul>
-                                <a className="navbar--dropdown--item" href="https://drive.google.com/file/d/1DTjJAUafmm6EpU492lKeAlrbF4bMtAxw/view?usp=sharing" target="_blank" rel="noopener noreferrer">
-                                <li>Resume</li>
-                            </a>
-                            <a className="navbar--dropdown--item" href="https://www.linkedin.com/in/maanas-gopi" target="_blank" rel="noopener noreferrer">
-                                <li>LinkedIn</li>
-                            </a>
-                            <a className="navbar--dropdown--item" href="https://github.com/MaanasG" target="_blank" rel="noopener noreferrer">
-                                <li>GitHub</li>
-                            </a>
-                        </ul>
-                    </Slide>
+                    <ul>
+                            <a className="navbar--dropdown--item" href="https://drive.google.com/file/d/1DTjJAUafmm6EpU492lKeAlrbF4bMtAxw/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                            <li>Resume</li>
+                        </a>
+                        <a className="navbar--dropdown--item" href="https://www.linkedin.com/in/maanas-gopi" target="_blank" rel="noopener noreferrer">
+                            <li>LinkedIn</li>
+                        </a>
+                        <a className="navbar--dropdown--item" href="https://github.com/MaanasG" target="_blank" rel="noopener noreferrer">
+                            <li>GitHub</li>
+                        </a>
+                    </ul>
                 </div>
             </div>
         </nav>
